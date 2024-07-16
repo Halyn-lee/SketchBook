832ba5fec914d994cf57e792b9abccec79bd7e78
Halyn-lee <leehalyn@gmail.com>
2024-07-16 10:57:53 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/app/sketchbook/SketchBookApplication.java
@@ -0,0 +1,13 @@
+package com.app.sketchbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SketchBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SketchBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/sketchbook/SketchBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sketchbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SketchBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
