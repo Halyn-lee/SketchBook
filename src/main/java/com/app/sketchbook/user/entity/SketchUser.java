e553da84ba04c770e5c8d2a21cfabf234a50571a
hssoomin <hssoomin@gmail.com>
2024-07-26 17:30:59 +0900

    add:친구 기능 추가

--- src/main/java/com/app/sketchbook/user/entity/SketchUser.java
+++ src/main/java/com/app/sketchbook/user/entity/SketchUser.java
@@ -39,4 +39,5 @@ public class SketchUser {
 //
     private String password;
 
+    private boolean profile_public = true;
 }
--- src/main/java/com/app/sketchbook/user/repository/UserRepository.java
+++ src/main/java/com/app/sketchbook/user/repository/UserRepository.java
@@ -3,7 +3,11 @@ package com.app.sketchbook.user.repository;
 import com.app.sketchbook.user.entity.SketchUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<SketchUser, Long> {
     SketchUser findByEmailAndSocial(String email, String social);
 
+    List<SketchUser> findByUsernameContainingIgnoreCase(String keyword);
+    List<SketchUser> findByEmailContainingIgnoreCase(String keyword);
 }
